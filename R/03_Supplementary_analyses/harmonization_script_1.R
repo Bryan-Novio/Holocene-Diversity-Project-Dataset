1981866e4faceeec2362be69592fc4c0413f1228
Bryan Novio <noviobryan@gmail.com>
2025-07-14 10:18:18 +0200

    update_harmonization_scripts

--- R/03_Supplementary_analyses/harmonization_script_1.R
+++ R/03_Supplementary_analyses/harmonization_script_1.R
@@ -4,130 +4,46 @@ library(purrr)
 library(dplyr)
 library(taxospace)
 
-
 #----------------------------------------------------------#
 # 1. Load data files per dataset (study)------------------
 #----------------------------------------------------------#
 
 method_data_files <- list.files("Data/Processed/Other/", pattern ="[.]rds$", full.names = TRUE)
-method_data_files
+taxa_ref_table    <- readr::read_csv(here("Data/Input/Harmonisation_tables/taxa_reference_table_2025-01-24.csv"), show_col_types = FALSE)
 
 #----------------------------------------------------------#
-# 2. Select unique from all dataset from each study ------
+# 2. Select unique taxa from all dataset from each study ------
 #----------------------------------------------------------#
 
-method_data <- purrr::map(method_data_files,readr::read_rds) %>% 
-  bind_rows() %>%  
-  distinct(taxa)
-
-method_data_un <- unlist(method_data)
+method_data_taxa <- purrr::map(method_data_files,readr::read_rds) %>% bind_rows() %>% 
+  distinct(taxa) %>% rename(taxon_name = taxa)
 
 #----------------------------------------------------------#
-# 3. Fix format of taxon names (i.e. capitalize first letter) 
+# 3. Convert taxon names to neotoma names
 #----------------------------------------------------------#
 
-method_data_vec <- as.vector.data.frame(method_data_un)
-method_data_cap <- str_to_sentence(method_data_vec)
-method_data_form <- str_replace_all(method_data_cap,"_"," ")
-str(method_data_form)
-method_data_form_tib <- tibble(method_data_form)
-method_data_re <- rename(method_data_form_tib, taxon_name = method_data_form)
+method_data_join_neotoma <- inner_join(method_data_taxa,taxa_ref_table, by ='taxon_name')
+
+method_data_join_neotoma_re <- method_data_join_neotoma %>% select(neotoma_names) %>% rename(taxon_name = neotoma_names)
+
+method_data_vec <- as.vector.data.frame(method_data_join_neotoma_re)
+
+method_data_vec_chr <- unlist(method_data_vec) %>% as.character()
+str(method_data_vec_chr)
 
 #----------------------------------------------------------#
-# 4. Load taxa_ref_table
+# 4. Get classification per taxon list 
 #----------------------------------------------------------#
-
-taxa_ref_table <- readr::read_csv(here("Data/Input/Harmonisation_tables/taxa_reference_table_2025-01-24.csv"))
-
-
-#----------------------------------------------------------#
-# 5. Translate "taxa" to Neotoma taxa names
-#----------------------------------------------------------#
-
-neo_taxa <- taxa_ref_table %>% 
-  semi_join(method_data_re, join_by(neotoma_names == taxon_name)) %>% # select neotoma_names that match taxon name from all studies
-  select(neotoma_names)
-
-neo_taxa
-neo_taxa_re <- rename(neo_taxa, taxon_name = neotoma_names)
-
-str(neo_taxa_re)
-
-neo_taxa_vec<- unlist(neo_taxa_re) %>% as.character()
-
-neo_taxa_vec
-str(neo_taxa_vec)
-
-neo_taxa_vec[72]
-neo_taxa_vec_1 <- neo_taxa_vec[-c(72:90)]
-get_classification(taxa_vec = neo_taxa_vec[72], use_only_exact_match = FALSE)
-
-#----------------------------------------------------------#
-# 6. Get classification per taxon list all the same time 
-#----------------------------------------------------------#
-
-# with error
-
-method_data_class <- 
-  purrr::map(.x = neo_taxa_vec_1,
-             .f = ~ get_classification(taxa_vec = .x,
-               use_only_exact_match = FALSE),
-                .progress = TRUE) 
-
-method_data_class
-method_data_class[[30]]$classification
-
-# this one is working
 
 safe_classification <- purrr::safely(get_classification)   # capture errors 
-class_taxa <- purrr::map(neo_taxa_vec, ~ safe_classification
+class_taxa <- purrr::map(method_data_vec_chr, ~ safe_classification
                          (taxa_vec = .x, use_only_exact_match = FALSE), 
                            .progress = TRUE) 
 
-class_taxa
 
-class_taxa[[99]]                        # succeeded
-class_taxa[[99]]$result
-class_taxa[[99]]$result$classification
+class_taxa_tib <- tibble::tibble(class_taxa)  # classification as tibble
+class_taxa_tib %>% unnest(class_taxa)
+View(class_taxa_tib)
 
-class_taxa[[72]]                         # failed
-class_taxa[[72]]$result
-class_taxa[[72]]$result$classification
-
-class_taxa[[617]]                        # failed
-class_taxa[[617]]$result
-class_taxa[[617]]$result$classification
-
-class_taxa$result$classification
-
-class_taxa[[618]]                        # plant/animal
-class_taxa[[618]]$result
-class_taxa[[618]]$result$classification
-
-
-#----------------------------------------------------------#
-# 7. Filter out if the classification is plant or an animal 
-#----------------------------------------------------------#
-
-# Extract names where classification includes 'Animalia'
-
-class_with_animalia <- safely(function(x) {
-  classification_df <- x$result$classification[[1]]
-  if (!is.null(classification_df) && "name" %in% colnames(classification_df)) {
-    if ("Animalia" %in% classification_df$name) {
-      return(x$result$sel_name)
-    }
-  }
-  return(NA_character_)
-})
-
-class_with_animalia_res <- map(class_taxa, class_with_animalia)
-
-View(class_with_animalia_res)
-
-class_with_animalia_list <- map(class_with_animalia_res, "result") %>%
-  flatten_chr() %>% 
-  discard(is.na)
-
-typeof(class_with_animalia_list)
-
+class_taxa <- read_rds(here("Data/Processed/Data_harmonised/classified_taxa_neotoma_paper_1.rds"))
+write_rds(class_taxa, here("Data/Processed/Data_harmonised/classified_taxa_neotoma_paper_1.rds"))
--- R/03_Supplementary_analyses/harmonization_script_2.R
+++ R/03_Supplementary_analyses/harmonization_script_2.R
@@ -1,48 +1,79 @@
-
-
-#----------------------------------------------------------#
-# 1. Classification per taxon list all the same time 
-#----------------------------------------------------------#
-
-method_data_class <- 
-  purrr::map(.x = neo_taxa_vec_1,
-             .f = ~ get_classification(taxa_vec = .x,
-                                       use_only_exact_match = FALSE),
-             .progress = TRUE) 
-
-method_data_class
-method_data_class[[30]]$classification
-
-
-safe_classification <- purrr::safely(get_classification)   # capture errors 
-class_taxa <- purrr::map(neo_taxa_vec, ~ safe_classification
-                         (taxa_vec = .x, use_only_exact_match = FALSE), 
-                         .progress = TRUE) 
-
-class_taxa
-
-View(class_taxa)
-
-#----------------------------------------------------------#
-# 2. Failed classification 
-#----------------------------------------------------------#
-
+library(tidyverse)
+library(here)
 library(purrr)
-
-# Get index positions of failed classifications
-
-failed_indices <- keep(seq_along(class_taxa), function(i) {
-  class_df <- class_taxa[[i]]$result$classification[[1]]
-  is.null(class_df) || !inherits(class_df, "data.frame") || nrow(class_df) == 0 # conditions: (class_df) is either NULL, not a data frame, or an empty data frame
-})
+library(dplyr)
 
 #----------------------------------------------------------#
-# 3. Remove failed classification 
+# 1. Load data classified taxa rds file  ------------------
 #----------------------------------------------------------#
 
-class_taxa_success <- class_taxa[-c(failed_indices)] # 1401-30 = 1370 successful classifications
-View(class_taxa_success)
+class_taxa <- read_rds(here("Data/Processed/Data_harmonised/classified_taxa_neotoma_paper_1.rds"))
+
+class_taxa_tib <- tibble::tibble(class_taxa)  # classification as tibble
+class_taxa_tib %>% unnest(class_taxa)
+View(class_taxa_tib)
+
+
+#----------------------------------------------------------#
+# 2. Identify taxa with successful or failed classifications
+#----------------------------------------------------------#
+
+class_taxa_tib_has_no_result <- function(x) {   # function to id taxon with failed classifications
+  is.null(x$result)
+}
+
+class_taxa_checked <-                           # produce checklist of successful or failed classifications: TRUE -> failed
+  class_taxa_tib %>% 
+  dplyr::mutate(
+    has_no_results = purrr::map_lgl(
+      .x = class_taxa,.f =
+        class_taxa_tib_has_no_result
+    )
+  ) %>%  print(n = 500)
+
+class_taxa_has_no_res <- class_taxa_checked %>% select(has_no_results)  # select on classification results column as lgl
+View(class_taxa_has_no_res)
+
+
+class_taxa_has_no_res_chr <- class_taxa_has_no_res %>% as_vector() %>% as.character() %>% as_tibble() # as chr
+typeof(class_taxa_has_no_res_chr)
+
+
+class_taxa_names <- bind_cols(method_data_join_neotoma_re,class_taxa_has_no_res_chr) %>% rename(class_res = value)                       #bind neotoma_name with classification results 
+
+class_taxa_failed <- class_taxa_names %>% filter(class_res == TRUE)   # list of failed classifications
+class_taxa_success <- class_taxa_names %>% filter(class_res == FALSE)   # list of failed classifications
+#----------------------------------------------------------#
+# 3. Filter out if the classification is plant or an animal 
+#----------------------------------------------------------#  
+
+class_taxa_succeeded <- class_taxa_checked %>% # successful classification with tabulate results
+  filter(has_no_results == FALSE) %>% 
+  unnest(class_taxa) %>% 
+  unnest_longer(class_taxa) %>% 
+  unpack(cols = class_taxa) %>% 
+  unnest_longer(class_taxa$classification) %>% 
+  filter(!is.na(id))
+
+
+class_taxa_animals <- class_taxa_succeeded %>%  # with classification Animalia 
+  select(sel_name,classification) %>% 
+  unnest_wider(classification) %>% 
+  select(sel_name, name, rank) %>% 
+  unnest(c(name,rank)) %>% 
+  filter(rank == "kingdom") %>% 
+  filter(name == "Animalia")
 
 
 
+class_taxa_plants <- class_taxa_succeeded %>%  # with classification Plantae 
+  select(sel_name,classification) %>% 
+  unnest_wider(classification) %>% 
+  select(sel_name, name, rank) %>% 
+  unnest(c(name,rank)) %>% 
+  filter(rank == "kingdom") %>% 
+  filter(name == "Plantae")
 
+
+write_rds(class_taxa_succeeded, here("Data/Processed/Data_harmonised/class_taxa_succeeded.rds"))
+write_csv(class_taxa_plants, here("Data/Processed/Data_harmonised/class_taxa_plants.csv"))
