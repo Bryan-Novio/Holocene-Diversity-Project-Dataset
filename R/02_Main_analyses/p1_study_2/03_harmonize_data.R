7d200b6f3e9a90ba49db60f04d24df522ed4c5e7
OndrejMottl <ondrej.mottl@gmail.com>
2025-10-20 15:17:25 +0200

    Harmonisation: fix the harmonisation function and delete the redundnat one; fix the script to use correct data

---
+++ R/02_Main_analyses/p1_study_2/03_harmonize_data.R
@@ -0,0 +1,66 @@
+#----------------------------------------------------------#
+#               Holocene Diversity Project
+#
+#            Paper01| Method 2: Simova et al
+#
+#
+#                          2023
+# North America, site-based richness (dataset_id,age,
+# 1000 bins - rarefy 400
+#
+#                   ---HARMONIZATION ----
+#
+#----------------------------------------------------------#
+
+library(tidyverse)
+library(here)
+
+#----------------------------------------------------------#
+# 1. Load data set -----------------------------------------
+#----------------------------------------------------------#
+
+data_only_woody <- read_csv(
+  here("Data/Processed/Other/data_only_woody.csv")
+) # 99 distinct pollen_type
+hamronisation_table <- readr::read_csv(
+  here::here("Data/harmonization_table_rev.csv")
+)
+
+#----------------------------------------------------------#
+# 2. Load functions ---------------------------------------
+#----------------------------------------------------------#
+
+# Get a vector of general functions
+
+fun_list <-
+  list.files(
+    path = "R/Functions/",
+    pattern = "\\.R$",
+    recursive = TRUE
+  )
+
+# Load the function into the global environment
+
+source_files <- sapply(
+  paste0("R/Functions/", fun_list, sep = ""),
+  source
+)
+
+#----------------------------------------------------------#
+# 3. Test {harmonize_taxa} at different taxo rank --
+#----------------------------------------------------------#
+
+# Harmonize taxa at different taxonomic levels
+
+data_study2_harmonised <-
+  harmonize_taxa(
+    data_to_harmonize = data_only_woody,
+    harmomnisation_table = hamronisation_table,
+    level = "level_6"
+  )
+
+#----------------------------------------------------------#
+# Write the harmonized data to RDS files
+
+write_rds(data_study2_harmonised, here("Outputs/Data/paper_1_study_2/data_study2_harmonised.rds"))
+#----------------------------------------------------------#
--- R/Functions/harmonize_taxa.R
+++ R/Functions/harmonize_taxa.R
@@ -1,29 +1,35 @@
+harmonize_taxa <- function(data_to_harmonize, harmomnisation_table, level) {
+  assertthat::assert_that(
+    is.data.frame(data_to_harmonize),
+    msg = "data_to_harmonize has to be data.frame"
+  )
 
-harmonize_taxa <- function(data_to_harmonize, data_ancillary, level) {
-  
-  taxa_level <- sym(level)  # Convert string to symbol for use in dplyr
-  
-  data_ancillary <- list(neotoma_taxa = neotoma_taxa,
-                         harmonization_table = harmonization_table)
-  
-  prep_data_study_ages_pollen <- data_to_harmonize %>%  join_data_prep_ages_pollen()
-  
-  data_to_harmonize %>%
-    pull(taxa) %>%
-    unique() %>%
-    tibble::tibble() %>%
-    as_vector() %>%
-    as_tibble() %>%
-    rename(taxon_name = value) %>%
-    inner_join(neotoma_taxa, by = "taxon_name") %>%
-    select(neotoma_names) %>%
-    rename(taxon_name = neotoma_names) %>%
-    inner_join(harmonization_table, by = "taxon_name") %>%
-    select(taxon_name, level_5, level_6, level_7, level_8) %>%
-    inner_join(prep_data_study_ages_pollen, by = "taxon_name", relationship = "many-to-many") %>%
-    group_by(dataset_id, sample_id, age, !!taxa_level) %>%
-    summarize(pollen_sum = sum(pollen_counts), .groups = "drop") %>% 
-    rename(taxa = starts_with("level_")) %>% 
-    rename(pollen_counts = pollen_sum) %>% 
-    drop_na()
+  assertthat::assert_that(
+    all(c("dataset_id", "sample_id", "age", "taxon_name", "pollen_counts") %in% colnames(data_to_harmonize)),
+    msg = "data must contain columns 'dataset_id', 'sample_id', 'age', 'taxon_name', 'pollen_counts' "
+  )
+
+  taxa_level <- sym(level) # Convert string to symbol for use in dplyr
+
+  harmomnisation_table_prep <-
+    dplyr::select(taxon_name, !!taxa_level) %>%
+    rlang::set_names(
+      nm = c("taxon_name", "taxa_harmonised")
+    )
+
+  res <-
+    data_to_harmonize %>%
+    dpylr::inner_join(
+      harmomnisation_table_prep,
+      by = "taxon_name"
+    ) %>%
+    dpylr::group_by(dataset_id, sample_id, age, taxa_harmonised) %>%
+    dpylr::summarize(
+      pollen_sum = sum(pollen_counts),
+      .groups = "drop"
+    ) %>%
+    dpylr::rename(taxa = taxa_harmonised) %>%
+    dpylr::rename(pollen_counts = pollen_sum)
+
+  return(res)
 }
