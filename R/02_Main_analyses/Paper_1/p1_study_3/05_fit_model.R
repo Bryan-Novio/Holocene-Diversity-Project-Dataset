e54204208778a4c3e2fa482d58a00ba2ba6f2374
Bryan-Novio <164608451+Bryan-Novio@users.noreply.github.com>
2026-02-10 14:27:59 +0100

    Fix modelling study 3 (#28)
    * -add column “region” to all regions (continents) data.frames
    
    -Bind rows from all regions (continents)
    
    * fit gam with scaled richness and back-transform
    
    * polish gam plotting, add some aesthetics
    
    * fix modelling for study3
    
    * revised study3 modelling using updated harmonised datasets
    
    * update study3 modelling

--- R/02_Main_analyses/Paper_1/p1_study_3/05_fit_model.R
+++ R/02_Main_analyses/Paper_1/p1_study_3/05_fit_model.R
@@ -46,50 +46,44 @@ richness_data_europe <-
 richness_data_namerica <- 
   read_csv(here("Data/Paper_1/data_estimate_richness/study3_richness_namerica.csv"))
 
-#  Convert dataset_id as random factor
+#  add regions to each dataset
 
 richness_data_asia <-
   richness_data_asia %>%              
-  mutate(region = "asia")
-
+  mutate(region = "asia") 
 
 richness_data_europe <-
-  richness_data_europe %>%              
-  mutate(dataset_id = as_factor(dataset_id))
-
+  richness_data_europe %>% 
+  mutate(region = "europe")
 
 richness_data_namerica <-
-  richness_data_namerica %>%              
+  richness_data_namerica %>%  
+  mutate(region = "namerica") 
+
+#bind all dataframes with richness estimate, convert dataset_id as random factor and add regions
+
+study3_richness <- 
+  bind_rows(richness_data_asia,richness_data_europe, richness_data_namerica) %>%
   mutate(dataset_id = as_factor(dataset_id))
 
 # standardize diversity use scale():
 
-richness_data_asia_std  <- 
-  richness_data_asia$richness %>% scale() %>% 
-  as_tibble()
+study3_richness_z_scores <- 
+  study3_richness %>% 
+  group_by(region) %>% 
+  mutate(st_richness = scale(richness)) %>%
+  ungroup() %>% 
+  mutate(st_richness = st_richness[,1]) %>% 
+  mutate(region = as_factor(region))
 
-richness_data_m <- bind_cols(richness_data_europe,richness_data_asia, richness_data_namerica) %>%
-  mutate(st_richness = scale(richness))
+# Get mean and sd to back-transform 
 
-
-
-p <-
-  ggplot2::ggplot(
-    richness_data_asia_std_2,
-    ggplot2::aes(x = age, y = st_richness )
-  ) +
-  ggplot2::labs(
-    y = "Pollen Richness", x = "Age") +
-  ggplot2::theme_classic(
-    
-  )+
-  ggplot2::theme(legend.position = "none",
-                 plot.title = element_text(color = "#2a707f"),
-                 axis.title = element_text(color = "#2a707f", size = 18),
-                 axis.text  = element_text(color = "#2a707f", size = 18),
-                 axis.ticks = element_line(color = "#2a707f"),
-                 axis.line  = element_line(color = "#2a707f", linewidth = 1)
-  )
+study3_richness_standard <-
+  study3_richness %>% 
+  dplyr::group_by(region) %>% 
+  dplyr::summarise(mean_richness = mean(richness, na.rm = TRUE), 
+                   sd_richness = sd(richness, na.rm = TRUE)) %>% 
+  dplyr::ungroup()
 
 #----------------------------------------------------------#
 # 3. Model fitting -----
@@ -102,7 +96,7 @@ n_available_cores <-
 
 # number of cores to use cannot be more than number of random effect levels
 n_cores_to_use <-
-  richness_data_asia_std_2 %>%
+  study3_richness_z_scores %>%
   dplyr::distinct(dataset_id) %>%
   nrow() %>%
   {
@@ -114,15 +108,15 @@ n_cores_to_use <-
 
 set.seed(19900723)
 
-gam_1 <-
+gam_s3 <-
   fit_regression_model(
-    data = richness_data_asia_std_2,
-    y_var = "richness",
+    data = study3_richness_z_scores,
+    y_var = "st_richness",
     time_var = "age",
     group_var = "region",
-    random = "slope",
+    random = "study3",
     sel_k = 12, 
-    error_family = stats::poisson(link = "log"),
+    error_family = scat(),
     nthreads = n_cores_to_use,
     discrete = TRUE,
     control = mgcv::gam.control(
@@ -133,7 +127,7 @@ gam_1 <-
 
 ## 3.3. Save model as RDS files --
 
-write_rds(gam_1,here("Data/Paper_1/data_model/gam_2_asia.rds"))
+write_rds(gam_s3,here("Data/Paper_1/data_model/gam_3.rds"))
 
 #----------------------------------------------------------#
 # 4. Model prediction -----
@@ -141,80 +135,86 @@ write_rds(gam_1,here("Data/Paper_1/data_model/gam_2_asia.rds"))
 
 data_dummy_full <-
   tidyr::expand_grid(
-    dataset_id = unique(richness_data_asia_std_2$dataset_id),
+    study3_richness_z_scores %>% 
+      distinct(region),
     age = seq(
-      min(richness_data_asia_std_2$age),
-      max(richness_data_asia_std_2$age),
-      length.out = 10
-    )
-  )
-
-data_dummy_general <-
-  tidyr::expand_grid(
-    age = seq(
-      min(richness_data_asia_std_2$age),
-      max(richness_data_asia_std_2$age),
-      length.out = 10
+      min(study3_richness_z_scores$age),
+      max(study3_richness_z_scores$age),
+      length.out = 100
     )
   )
 
 data_pred_full <-
   predict_model(
-    model = gam_1,
+    model = gam_s3,
     newdata = data_dummy_full,
-    type = "response"
-  ) %>%
-  as.data.frame() %>%
-  tibble::as_tibble() %>%
-  dplyr::relocate(
-    estimate, dataset_id, age,
-    .before = dplyr::everything()
-  )
-
-data_pred_general <-
-  predict_model(
-    model = gam_1,
-    newdata = data_dummy_general,
     type = "response",
     exclude_terms = "dataset_id"
   ) %>%
   as.data.frame() %>%
   tibble::as_tibble() %>%
-  dplyr::mutate(
-    dataset_id = NA_character_
-  ) %>%
   dplyr::relocate(
-    estimate,age,
+    estimate, region, age,
     .before = dplyr::everything()
   )
+
+data_richness_btr <- 
+  data_pred_full %>% 
+  left_join(study3_richness_standard, by = "region") %>% 
+  mutate(
+    richness = estimate*sd_richness + mean_richness,
+    rich_low = conf_low*sd_richness + mean_richness,
+    rich_high = conf_high*sd_richness + mean_richness) 
+  
 
 #----------------------------------------------------------#
 # 4. Visualization -----
 #----------------------------------------------------------#
 
-#  4.1. Plot predictions for individual series-----
-p2 +
-  ggplot2::facet_wrap(~ dataset_id) +
+##general plot
+
+p <-
+  ggplot2::ggplot(
+  ) +
+  ggplot2::labs(
+    y = "Pollen Richness", x = "cal yr BP") +
+  ggplot2::theme_classic(
+    
+  )+
+  ggplot2::theme(legend.position = "none",
+                 plot.title = element_text(color = "#2a707f"),
+                 axis.title = element_text(color = "#2a707f", size = 18),
+                 axis.text  = element_text(color = "#2a707f", size = 24),
+                 axis.ticks = element_line(color = "#2a707f"),
+                 axis.line  = element_line(color = "#2a707f", linewidth = 1)
+  )
+
+#  4.1. Plot predictions for each region -----
+
+#show continental trend in one figure
+
+p1 <- p +
+  ggplot2::facet_wrap(~ region, dir = 'rt', ncol = 1, strip.position = 'right') +
   ggplot2::geom_ribbon(
-    data = data_pred_full,
+    data = data_richness_btr,
     ggplot2::aes(
       x = age,
-      y = estimate,
-      ymin = conf_low,
-      ymax = conf_high,
-      fill = dataset_id
+      y = richness,
+      ymin = rich_low,
+      ymax = rich_high,
+      fill = as.factor(region)
     ),
-    alpha = 0.1
+    alpha = 0.3
   ) +
   ggplot2::geom_line(
-    data = data_pred_full,
-    ggplot2::aes(x = age, y = estimate,color = dataset_id),
+    data = data_richness_btr,
+    ggplot2::aes(x = age, y = richness,color = region),
     linewidth = 1
   ) +
   ggplot2::theme(legend.position = "none",
                  axis.text  = element_text(size = 5),
                  strip.text = element_text(
-                   size = 6,
+                   size = 10,
                    color = "#2a707f"
                  ),
                  strip.background = element_rect(
@@ -224,40 +224,133 @@ p2 +
                  )
   ) +
   ggplot2::coord_cartesian(
-    ylim = c(0, max(richness_data_eu$richness) + 5)
-  )
+    ylim = c(14, 24) 
+  ) 
 
-# 4.2. Plot general trend-----
+p1 +
+  ggplot2::scale_x_reverse()
 
-p +
+
+## show individual trend for each continent
+
+asia <- data_richness_btr %>% filter(region == 'asia')
+europe <- data_richness_btr %>% filter(region == 'europe')
+namerica <- data_richness_btr %>% filter(region == 'namerica')
+
+##Asia
+
+A <- p +
   ggplot2::geom_ribbon(
-    data = data_pred_general,
+    data = asia,
     ggplot2::aes(
       x = age,
-      y = estimate,
-      ymin = conf_low,
-      ymax = conf_high
+      y = richness,
+      ymin = rich_low,
+      ymax = rich_high,
+      fill = region
     ),
-    fill = "#2a707f",
     alpha = 0.1
   ) +
   ggplot2::geom_line(
-    data = data_pred_general,
-    ggplot2::aes(x = age, y = estimate),
-    linewidth = 3,
-    color = "#2a707f"
+    data = asia,
+    ggplot2::aes(x = age, y = richness),
+    linewidth = 4, color = 'red'
   ) +
-  ggplot2::theme(
-    legend.position = "none"
+  ggplot2::theme(legend.position = "none",
+                 axis.text  = element_text(size = 22),
+                 strip.text = element_text(
+                   size = 16,
+                   color = "#2a707f"
+                 ),
+                 strip.background = element_rect(
+                   color = "#2a707f",
+                   fill = NA,
+                   linewidth = 0.3
+                 )
   ) +
-  ggplot2::coord_cartesian(
-    ylim = c(15, 17) ,
-    xlim = c(12000,0)
-  )+
-  ggplot2::scale_x_reverse(
-    limits = c(12000, 0),
-    breaks = seq(0, 12000, by = 4000)
-  )
+  ggplot2::coord_cartesian(ylim = c(14, 19) 
+  ) + 
+  ggplot2::scale_x_reverse()
 
+A
 
+#Europe
+
+E <- p +
+  ggplot2::geom_ribbon(
+    data = europe,
+    ggplot2::aes(
+      x = age,
+      y = richness,
+      ymin = rich_low,
+      ymax = rich_high,
+      fill = region
+    ),
+    alpha = 0.1
+  ) +
+  ggplot2::geom_line(
+    data = europe,
+    ggplot2::aes(x = age, y = richness),
+    linewidth = 4, color = 'purple'
+  ) +
+  ggplot2::theme(legend.position = "none",
+                 axis.text  = element_text(size = 22),
+                 strip.text = element_text(
+                   size = 10,
+                   color = "#2a707f"
+                 ),
+                 strip.background = element_rect(
+                   color = "#2a707f",
+                   fill = NA,
+                   linewidth = 0.3
+                 )
+  ) +
+  ggplot2::coord_cartesian(ylim = c(14, 22) 
+  ) + 
+  ggplot2::scale_x_reverse()
+
+E
+
+##NAmerica
+
+N <- p +
+  ggplot2::geom_ribbon(
+    data = namerica,
+    ggplot2::aes(
+      x = age,
+      y = richness,
+      ymin = rich_low,
+      ymax = rich_high,
+      fill = region
+    ),
+    alpha = 0.1
+  ) +
+  ggplot2::geom_line(
+    data = namerica,
+    ggplot2::aes(x = age, y = richness),
+    linewidth = 4, color = 'orange'
+  ) +
+  ggplot2::theme(legend.position = "none",
+                 axis.text  = element_text(size = 22),
+                 strip.text = element_text(
+                   size = 10,
+                   color = "#2a707f"
+                 ),
+                 strip.background = element_rect(
+                   color = "#2a707f",
+                   fill = NA,
+                   linewidth = 0.3
+                 )
+  ) +
+  ggplot2::coord_cartesian(ylim = c(16, 18) 
+  ) + 
+  ggplot2::scale_x_reverse()
+
+N
+
+##combine continental trends into single plot
+
+library(patchwork)
+
+A + E + N
 
