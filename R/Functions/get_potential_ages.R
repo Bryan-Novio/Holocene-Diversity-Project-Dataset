9c7ef3961dce111667d5360debbc017602d13ead
Bryan Novio <noviobryan@gmail.com>
2026-02-23 13:41:36 +0100

    updated get_potential_ages() function

--- R/Functions/get_potential_ages.R
+++ R/Functions/get_potential_ages.R
@@ -5,7 +5,7 @@ get_potential_ages <-
     
     data_uncertainty_potential_ages <-
       data_source %>%
-      mutate(age_uncertainty = map(age_uncertainty, ~ {
+      mutate(age_uncertainty = purrr::map(age_uncertainty, ~ {
         # Convert the matrix to a long data frame
         as.data.frame(.x) %>%
           pivot_longer(cols = everything(), 
