1d9a92f092260876e86adf5724b51c4578377328
Bryan Novio <noviobryan@gmail.com>
2026-02-23 13:43:19 +0100

    interchanged rarefaction and binning; ensured each step done @ 1k iterations

--- R/02_Main_analyses/Paper_1/p1_study_3/02_rarefy_data.R
+++ R/02_Main_analyses/Paper_1/p1_study_3/02_rarefy_data.R
@@ -20,6 +20,9 @@ library(vegan)
 # 1. Load data set -----------------------------------------
 #----------------------------------------------------------# 
 
+data <-
+  read_rds(here("Outputs/Data/data_assembly_2025-03-14__796c6bc270edcf0a682242164dd28a39__.rds"))
+
 data_binned_asia <- 
   read_rds(here("Data/Paper_1/data_bin/data_study3_binned_asia.rds"))
 
@@ -46,6 +49,7 @@ fun_list <-
     pattern = "\\.R$",
     recursive = TRUE
   )
+
 
 # Load the function into the global environment
 
@@ -83,29 +87,12 @@ rarefied_data_asia <-
 
 # do repetitive rarefaction 1000 times
 
-n_iter <- 1:3
-
-##within script
-
 rarefied_dataset_assembly_asia <- 
-  purrr::map(n_iter, function(x) {
-  
-  rarefied_data <- data_to_rarefy_asia %>% 
-    rarefy_all_samples(n_grains = 300) 
-  
-tibble(
-    id = as.character(x), # Ensures ID is character
-    rarefied_dataset = list(rarefied_data) # Wraps data in a list-column
-  ) 
-}) %>% purrr::list_rbind()
+  data_to_rarefy_asia %>% 
+  rarefy_all_samples_iter(n_iter = 1000)
 
 
-##using a function
-
-rarefied_dataset_assembly2 <- 
-  rarefy_all_samples_iter(data_rarefy = data_to_rarefy_asia,
-                          n_iter = n_iter)
-
+rarefied_dataset_assembly_asia %>% filter(id == "1") %>% unnest(rarefied_dataset)
 
 ## Europe
 
@@ -128,21 +115,11 @@ rarefied_data_europe <-
     data_source = data_to_rarefy_europe,
     n_grains = 300)
 
-##with iteration
-
+##with iteration 1000x
 
 rarefied_dataset_assembly_europe <- 
-  purrr::map(n_iter, function(x) {
-    
-    rarefied_data <- data_to_rarefy_europe %>% 
-      rarefy_all_samples(n_grains = 300)
-    
-    tibble(
-      id = as.character(x), 
-      rarefied_dataset = list(rarefied_data) 
-    )
-  }) %>% 
-  list_rbind()
+  data_to_rarefy_europe%>% 
+  rarefy_all_samples_iter(n_iter = 1000)
 
 ##NAmerica
 
@@ -165,27 +142,101 @@ rarefied_data_namerica <-
     data_source = data_to_rarefy_namerica,
     n_grains = 300)
 
-##with iteration
+##with iteration 1000x
 
 rarefied_dataset_assembly_namerica <- 
-  purrr::map(n_iter, function(x) {
-    
-    rarefied_data <- data_to_rarefy_namerica %>% 
-      rarefy_all_samples(n_grains = 300)
-    
-    tibble(
-      id = as.character(x), 
-      rarefied_dataset = list(rarefied_data)
-    )
-  }) %>% 
-  list_rbind()
+  data_to_rarefy_namerica %>% 
+  rarefy_all_samples_iter(n_iter = 1000)
+
+
+#----------------------------------------------------------#
+# 4. Add random selection of time  to each iteration ------
+#----------------------------------------------------------#
+
+##extract age uncertainties from full dataset
+
+data_age_uncertainty <- 
+  data %>% 
+  select(dataset_id, age_uncertainty)
+
+data_age_uncertainty %>% 
+  filter(dataset_id =="1001") %>% 
+  unnest(age_uncertainty)
+
+data_potential_ages <- 
+  get_potential_ages(data_age_uncertainty) #using a function
+
+## test rarefied_data_iter
+
+rarefied_dataset_assembly_asia <- 
+  read_rds(here("Data/Paper_1/data_rarefy/rarefied_dataset_assembly_asia.rds")) %>% rename(iter  = id)
+
+# collapse rarefied data assembly to single dataframe
+
+rarefied_dataset_assembly_asia_un <-
+  rarefied_dataset_assembly_asia %>%
+  unnest(cols = c(rarefied_dataset)) %>% 
+  separate_wider_delim(.,cols = dataset_id_age,
+                       names = c("dataset_id", "bin"),delim ="_") %>% 
+  select(-bin)
+
+# Add random selection of time to rarefied dataset with iterations
+
+##dataset_ids from assembly
+
+dataset_assembly_ids <-                 
+  rarefied_dataset_assembly_asia_un %>% 
+  distinct(dataset_id) %>% 
+  as.vector()
+
+## filter potential ages for dataset ids in the assembly
+
+data_potential_ages_filtered <- 
+  data_potential_ages %>%
+  filter(dataset_id %in% dataset_assembly_ids$dataset_id)
+
+## add random ages for each dataset_id in the assembly
+
+rarefied_dataset_assembly_asia_p_ages <- 
+  purrr::map(dataset_assembly_ids$dataset_id, 
+             ~ add_random_ages(., rarefied_dataset_assembly_asia_un,   data_potential_ages_filtered)) %>% 
+  list_rbind() %>% 
+  mutate(potential_age  = as.double(potential_age))
+
+## transform data for binning
+
+rarefied_dataset_assembly_asia_p_ages_to_bin <- 
+  rarefied_dataset_assembly_asia_p_ages %>%
+  pivot_longer(cols = -c(potential_age,iter,dataset_id),
+               names_to = "taxa",
+               values_to = "pollen_counts") %>% 
+  rename(age = potential_age) %>% 
+  mutate(pollen_counts  = as.double(pollen_counts))
 
 #----------------------------------------------------------#
 # 4. Write the rarefied data to an RDS file --------------
 #----------------------------------------------------------#
+
+##rarefied data multiple iteration (1000x)
+
+write_rds(rarefied_dataset_assembly_asia, here("Data/Paper_1/data_rarefy/rarefied_dataset_assembly_asia.rds"))
+
+write_rds(rarefied_dataset_assembly_europe, here("Data/Paper_1/data_rarefy/rarefied_dataset_assembly_europe.rds"))
+
+write_rds(rarefied_dataset_assembly_namerica, here("Data/Paper_1/data_rarefy/rarefied_dataset_assembly_namerica.rds"))
+
+
+## rarefied_data_assembly for binning
+
+write_rds(rarefied_dataset_assembly_asia_p_ages_to_bin, here("Data/Paper_1/data_rarefy/rarefied_dataset_assembly_asia_p_ages_to_bin.rds"))
+
+
+##rarefied data single iteration
 
 write_rds(rarefied_data_asia, here("Data/Paper_1/data_rarefy/data_study3_rarefied_asia.rds"))
 
 write_rds(rarefied_data_europe, here("Data/Paper_1/data_rarefy/data_study3_rarefied_europe.rds"))
 
 write_rds(rarefied_data_namerica, here("Data/Paper_1/data_rarefy/data_study3_rarefied_namerica.rds"))
+
+
--- R/02_Main_analyses/Paper_1/p1_study_3/03_bin_data.R
+++ R/02_Main_analyses/Paper_1/p1_study_3/03_bin_data.R
@@ -19,6 +19,9 @@ library(here)
 # 1. Load data set -----------------------------------------
 #----------------------------------------------------------# 
 
+rarefied_dataset_assembly_asia_p_ages_to_bin <- 
+  read_rds(here("Data/Paper_1/data_rarefy/rarefied_dataset_assembly_asia_p_ages_to_bin.rds"))
+
 data <-
   read_rds(here("Outputs/Data/data_assembly_2025-03-14__796c6bc270edcf0a682242164dd28a39__.rds"))
 
@@ -52,22 +55,9 @@ source_files <-
   paste0("R/Functions/", fun_list, sep = ""),
   source
 )
-#----------------------------------------------------------#
-# 3. Add random selection of time  to each iteration ------
-#----------------------------------------------------------#
-
-##extract age uncertainties from full dataset
-
-data_age_uncertainty <- 
-  data %>% 
-  select(dataset_id, age_uncertainty)
-
-
-data_potential_ages <- 
-  get_potential_ages(data_source = data_age_uncertainty) #using a function
 
 #----------------------------------------------------------#
-# 4. Bin data  at different taxo rank --
+# 3. Bin data  at different taxo rank --
 #----------------------------------------------------------# 
 
 data_binned_asia <-
@@ -82,9 +72,38 @@ data_binned_namerica <-
   data_study3_harmonised_namerica %>% 
   bin_data(dataset_id, 500)
 
+
+##Binning by 1000 iterations (runtime = 2-3 hrs.)
+
+
+iter <- unique(rarefied_dataset_assembly_asia_p_ages_to_bin$iter)
+
+data_binned_asia_res <- list()
+
+for (i in iter){
+  
+  data_binned_asia_res[[i]] <- 
+    rarefied_dataset_assembly_asia_p_ages_to_bin %>% 
+    filter(iter == i) %>% 
+    bin_data(.,dataset_id, 500)
+  
+}
+
+##transfrom back to a single dataframe
+
+data_binned_asia_res_new <- 
+  bind_rows(data_binned_asia_res,.id = "iter")
+
+
 #----------------------------------------------------------#
 # 4. Write the binned and prepared_data to RDS files
 #----------------------------------------------------------# 
+
+## binned rarefied data assembly (1000 iterations)
+
+write_rds(data_binned_asia_res_new, here("Data/Paper_1/data_bin/data_binned_asia_res_iter.rds"))
+
+## binned data single iteration
 
 write_rds(data_binned_asia, here("Data/Paper_1/data_bin/data_study3_binned_asia.rds"))
 write_rds(data_binned_europe, here("Data/Paper_1/data_bin/data_study3_binned_europe.rds"))
