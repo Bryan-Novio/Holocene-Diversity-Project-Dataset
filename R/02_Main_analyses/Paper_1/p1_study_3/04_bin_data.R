32b6a2ec7862378fa67560498165e80c766600c6
Bryan-Novio <164608451+Bryan-Novio@users.noreply.github.com>
2026-03-03 10:21:53 +0100

    revised study3 binning with iteration (#39)
    * revised study3 binning with iteration
    
    * hide checks when script is sourced

--- R/02_Main_analyses/Paper_1/p1_study_3/04_bin_data.R
+++ R/02_Main_analyses/Paper_1/p1_study_3/04_bin_data.R
@@ -19,21 +19,8 @@ library(here)
 # 1. Load data set -----------------------------------------
 #----------------------------------------------------------# 
 
-rarefied_dataset_assembly_asia_p_ages_to_bin <- 
-  read_rds(here("Data/Paper_1/data_rarefy/rarefied_dataset_assembly_asia_p_ages_to_bin.rds"))
-
-data <-
-  read_rds(here("Outputs/Data/data_assembly_2025-03-14__796c6bc270edcf0a682242164dd28a39__.rds"))
-
-data_study3_harmonised_asia <- 
-  read_rds(here("Data/Paper_1/data_harmonize/data_study3_harmonised_asia.rds"))
-
-
-data_study3_harmonised_europe <-
-  read_rds(here("Data/Paper_1/data_harmonize/data_study3_harmonised_europe.rds"))
-
-data_study3_harmonised_namerica <-
-  read_rds(here("Data/Paper_1/data_harmonize/data_study3_harmonised_namerica.rds"))
+data_with_new_age <-
+  read_rds(here("Data/Paper_1/data_rarefy/study3_rarefied_dataset_assembly_with_new_age.rds"))
 
 #----------------------------------------------------------#
 # 2. Load functions ---------------------------------------
@@ -56,54 +43,64 @@ source_files <-
   source
 )
 
+
 #----------------------------------------------------------#
-# 3. Bin data  at different taxo rank --
-#----------------------------------------------------------# 
+# 3. Reshape data to bin -------------------------
+#----------------------------------------------------------#
 
-data_binned_asia <-
-  data_study3_harmonised_asia %>% 
-  bin_data(dataset_id, 500)
-
-data_binned_europe <-
-  data_study3_harmonised_europe %>% 
-  bin_data(dataset_id, 500)
-
-data_binned_namerica <-
-  data_study3_harmonised_namerica %>% 
-  bin_data(dataset_id, 500)
-
-
-##Binning by 1000 iterations (runtime = 2-3 hrs.)
-
-
-iter <- unique(rarefied_dataset_assembly_asia_p_ages_to_bin$iter)
-
-data_binned_asia_res <- list()
-
-for (i in iter){
+dataset_with_new_age_to_bin <- 
+  data_with_new_age %>% 
+  mutate(data_to_bin = purrr::map(
+    .progress = TRUE,
+    .x = data_with_new_age,
+    .f = ~ {
+    data_to_bin <- 
+     .x %>%
+     unnest() %>% 
+     pivot_longer(cols = -c(sample_id,age,dataset_id),
+                     names_to = "taxa",
+                     values_to = "pollen_counts") %>% 
+        mutate(pollen_counts  = as.double(pollen_counts))
   
-  data_binned_asia_res[[i]] <- 
-    rarefied_dataset_assembly_asia_p_ages_to_bin %>% 
-    filter(iter == i) %>% 
-    bin_data(.,dataset_id, 500)
+  return(data_to_bin)
+ 
+    }
+  )
+)
   
+if(FALSE){
+  rlang::hash(dataset_with_new_age_to_bin$data_to_bin[[1]])
+  rlang::hash(dataset_with_new_age_to_bin$data_to_bin[[2]])
 }
 
-##transfrom back to a single dataframe
-
-data_binned_asia_res_new <- 
-  bind_rows(data_binned_asia_res,.id = "iter")
-
 #----------------------------------------------------------#
-# 4. Write the binned and prepared_data to RDS files
+# 4. Bin data  at different taxo rank --
 #----------------------------------------------------------# 
 
-## binned rarefied data assembly (1000 iterations)
+data_binned <-
+  dataset_with_new_age_to_bin %>% 
+  mutate(data_binned = purrr::map(
+    .progress = TRUE,
+    .x = data_to_bin,
+    .f = ~ {
+    binned <- 
+      .x %>% 
+      unnest() %>% 
+      bin_data (dataset_id, 500)
+    
+    return(binned)
+    
+    }
+   )
+  )
+ 
 
-write_rds(data_binned_asia_res_new, here("Data/Paper_1/data_bin/data_binned_asia_res_iter.rds"))
 
-## binned data single iteration
+#----------------------------------------------------------#
+# 5. Write the binned and prepared_data to RDS files
+#----------------------------------------------------------# 
 
-write_rds(data_binned_asia, here("Data/Paper_1/data_bin/data_study3_binned_asia.rds"))
-write_rds(data_binned_europe, here("Data/Paper_1/data_bin/data_study3_binned_europe.rds"))
-write_rds(data_binned_namerica, here("Data/Paper_1/data_bin/data_study3_binned_namerica.rds"))
+## binned rarefied data assembly (20 iterations)
+
+write_rds(data_binned, here("Data/Paper_1/data_bin/data_binned_iter20.rds"))
+
