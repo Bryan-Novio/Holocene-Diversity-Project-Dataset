7aef28c020b02fb219af7362d80b5d0231f392ac
Bryan-Novio <164608451+Bryan-Novio@users.noreply.github.com>
2026-03-10 10:17:06 +0100

    revised rarefy function to save each iteration to separate file (#40)
    * revised rarefy function to save each iteration to separate file
    
    * modified rarefy_all_samples_iter()

--- R/Functions/rarefy_all_samples_iter.R
+++ R/Functions/rarefy_all_samples_iter.R
@@ -1,15 +1,24 @@
-
+#' @title Rarefy dataset for iterations and save to drive
+#' @description
+#'We need to write to drive because otherwise we run out of memory
+#'  
 rarefy_all_samples_iter <- 
   
-  function(n_iter, data_to_rarefy){
+  function(n_iter, data_to_rarefy, path){
 
   1:n_iter %>% 
   purrr::set_names() %>% 
-  purrr::map(
+  purrr::walk(
     .progress = TRUE,
-    .f = ~ rarefy_all_samples(data_to_rarefy, n_grains = 300)
-    ) %>% 
-  dplyr::bind_rows(.id = "id")
+    .f = ~ rarefy_all_samples(data_to_rarefy, n_grains = 300) %>% 
+    readr::write_rds(file = paste0(path, "/", .x, ".rds"))
+    )
+    
+    # Explicitly clear local large objects and run gc()
+    rm(data)
+    gc(verbose = FALSE) 
+    
 }
     
+
 
