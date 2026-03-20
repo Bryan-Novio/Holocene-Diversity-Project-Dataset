925eb7f46eb1369578ff8fbaae848c423a280d0e
Bryan-Novio <164608451+Bryan-Novio@users.noreply.github.com>
2026-03-20 10:15:06 +0100

    updated binning for study3 (#46)
    * revised study3 binning with iteration
    
    * hide checks when script is sourced
    
    * updated binning with iterations
    
    * polished diversity estimation with iterations
    
    * add_new_ages function
    
    * updated git_ignore file
    
    * use bin_data_dt() in bin creation for study3
    
    * updated git_ignore
    
    * revised diversity estimation for study 3

--- R/02_Main_analyses/Paper_1/p1_study_3/05_estimate_richness.R
+++ R/02_Main_analyses/Paper_1/p1_study_3/05_estimate_richness.R
@@ -19,8 +19,11 @@ library(here)
 # 1. Load data set -----------------------------------------
 #----------------------------------------------------------# 
 
-data_binned <- 
-  read_rds(here("Data/Paper_1/data_bin/data_binned_iter20.rds"))
+paths <- list.files(
+  "Data/Paper_1/data_bin/bin_iterations",
+  pattern = "[.]rds$",
+  full.names = TRUE
+)
 
 region <- 
   read_rds(here("Data/Paper_1/data_subset/data_regions.rds"))
@@ -39,105 +42,80 @@ fun_list <-
   )
 
 # Load the function into the global environment
+
 source_files <-
   sapply(
   paste0("R/Functions/", fun_list, sep = ""),
   source
 )
+#----------------------------------------------------------#
+# 3. Output folder ----------
+#----------------------------------------------------------#
+
+out_dir <- 
+  here("Data/Paper_1/data_estimate_richness/s3_richness")
 
 #----------------------------------------------------------#
-# 3. Estimate richness  at different taxo rank -- at 12 cal yr bp 
+# 4. Estimate richness  at different taxo rank -- at 12 cal yr bp 
 #----------------------------------------------------------# 
 
-#3.1 Format rarefied data 
+for (i in seq_along(paths)) {
+  
+  # ---- Load binned dataset for each iteration  ----
+  data_binned <- readr::read_rds(paths[i])
+  
+  # Extract iteration id from filename
+  id <- as.integer(tools::file_path_sans_ext(basename(paths[i])))
+  
+  # ---- 4.1. Reshape rarefied dataset with new ages to bin ----
+  
+  data_binned_to_estimate <- 
+    data_binned %>% 
+          pivot_wider(names_from = taxa, 
+                      values_from = summed_pollen_count) %>% 
+          unite("dataset_id_age", dataset_id,
+                BIN, sep = "_", remove = TRUE)
 
-data_binned_to_estimate <- 
-  data_binned %>% 
-  dplyr::mutate(bin_to_estimate = purrr::map(
-    .progress = TRUE,
-    .x = data_binned,
-    .f = ~ {
-      .x %>% 
-    tidyr::unnest() %>% 
-    mutate(summed_pollen_count = as.integer(
-      summed_pollen_count)) %>% 
-        pivot_wider(names_from = taxa, 
-        values_from = summed_pollen_count) %>% 
-        select (!BIN_chr) %>% 
-        unite("dataset_id_age", dataset_id,
-        BIN, sep = "_", remove = TRUE)
-    }
-   )
-  )
+  # ---- 4.2. Prepare dataset for richness estimation ----
+  
+  data_binned_to_estimate_re <- 
+    data_binned_to_estimate %>% 
+          separate_wider_delim(dataset_id_age, delim = "_", 
+                               names = c("dataset_id","BIN")) %>% 
+          pivot_longer(cols = -c(dataset_id, BIN), 
+                       names_to = "taxa", values_to = "summed_pollen_count") %>% 
+          prepare_data_for_richness_estimation(type = "binned")
 
-data_binned_to_estimate_re <- 
-  data_binned_to_estimate %>% 
-  dplyr::mutate(bin_to_estimate = purrr::map(
-    .progress = TRUE,
-    .x = bin_to_estimate,
-    .f = ~ {
-      .x %>% 
-        tidyr::unnest() %>% 
-    separate_wider_delim(dataset_id_age, delim = "_", 
-    names = c("dataset_id","BIN")) %>% 
-    pivot_longer(cols = -c(dataset_id, BIN), 
-    names_to = "taxa", values_to = "summed_pollen_count") %>% 
-    prepare_data_for_richness_estimation(type = "binned")
-    }
-   )
-  )
+  # ---- 4.3.richness estimation for each binned data ----
+  
+  richness_estimate <- 
+    data_binned_to_estimate_re %>% 
+          estimate_richness() %>% 
+          mutate(age = as.numeric(age)) %>% 
+          mutate(dataset_id = as_factor(dataset_id)) %>% 
+          filter(age <= 12000)
+  
+  # ---- 4.4. add region to each binned data ----
+  
+  richness_estimate_re <- 
+    richness_estimate %>% 
+    ungroup() %>% 
+    mutate(dataset_id = as.character(dataset_id)) %>% 
+          inner_join(region, by = "dataset_id")
+  
+  readr::write_rds(richness_estimate_re,file = paste0(out_dir,"/",id,".rds"), compress = "gz" ) # Write the binned and prepared_data to RDS files
+  
+  rm(data_binned, id, data_binned_to_estimate, data_binned_to_estimate_re,richness_estimate,richness_estimate_re   )
+  
+  gc(verbose = FALSE)
+  
+}
 
-data_binned_to_estimate_re$bin_to_estimate[[1]]
+# View a single iteration
 
+one <- 
+  read_rds(here::here("Data/Paper_1/data_estimate_richness/s3_richness/1.rds"))
 
-#3.2. Estimate richness
+## end
 
-richness_estimate <- 
-  data_binned_to_estimate_re %>% 
-  dplyr::mutate(richness_estimate = purrr::map(
-    .progress = TRUE,
-    .x = bin_to_estimate,
-    .f = ~ {
-      .x %>% 
-        tidyr::unnest() %>% 
-        estimate_richness() %>% 
-        mutate(age = as.numeric(age)) %>% 
-        mutate(dataset_id = as_factor(dataset_id)) %>% 
-        filter(age <= 12000)
-    }
-  )
-)
-
-richness_estimate$richness_estimate[[1]]
-
-richness_estimate_re <- 
-  richness_estimate %>% 
-  select(id, richness_estimate) %>% 
-  mutate(richness_estimate_region = purrr::map(
-    .progress = TRUE,
-    .x = richness_estimate,
-    .f = ~ {
-      .x %>% 
-        unnest() %>% 
-        inner_join(region, by = "dataset_id")
-    }
-  )
-)
-
-richness_estimate_re$richness_estimate_region[[1]] %>% 
-  filter(region == "North America")
-
-richness_estimate_region <- 
-  richness_estimate_re %>% 
-  select(id, richness_estimate_region)
-
-richness_estimate_region$richness_estimate_region[[1]]
-
-summary(richness_estimate_region$richness_estimate_region[[1]])
-
-#----------------------------------------------------------#
-# 4. Write the richness data to an RDS file
-#----------------------------------------------------------# 
-
-write_rds(richness_estimate_region, here("Data/Paper_1/data_estimate_richness/study3_richness_estimate_region_iter.rds"))
 
