6f92d277ba9c31785bd2d0c507a70780754030bb
Bryan Novio <noviobryan@gmail.com>
2026-04-07 15:55:18 +0200

    save alternative rds files - fit model-study3

--- R/02_Main_analyses/Paper_1/p1_study_3/06_fit_model.R
+++ R/02_Main_analyses/Paper_1/p1_study_3/06_fit_model.R
@@ -34,69 +34,80 @@ source_files <-
   )
 
 #----------------------------------------------------------#
-# 2. Load data set -----------------------------------------
+# 2. Load richness  -----------------------------------------
 #----------------------------------------------------------# 
 
-richness_data_asia <- 
-  read_csv(here("Data/Paper_1/data_estimate_richness/study3_richness_asia.csv"))
+paths <- list.files(
+  "Data/Paper_1/data_estimate_richness/s3_richness",
+  pattern = "[.]rds$",
+  full.names = TRUE
+)
 
-richness_data_europe <- 
-  read_csv(here("Data/Paper_1/data_estimate_richness/study3_richness_europe.csv"))
+#check which iteration/s is age < 50; this is to achieve sel_k = 50 for model fit
 
-richness_data_namerica <- 
-  read_csv(here("Data/Paper_1/data_estimate_richness/study3_richness_namerica.csv"))
+check_age_50 <-   
+  purrr::map(
+    .x = paths,
+    .f = ~ {
+      .x %>% 
+        read_rds() %>% 
+        distinct(age) %>% 
+        count() 
+    }
+  ) %>% 
+  purrr::list_rbind() %>% 
+  filter(n >= 50)   #466 iters with distinct age < 50
 
-#  add regions to each dataset
+#----------------------------------------------------------#
+# 3. Output folders ----------
+#----------------------------------------------------------#
 
-richness_data_asia <-
-  richness_data_asia %>%              
-  mutate(region = "asia") 
+out_dir_mod <- 
+  here("Data/Paper_1/data_model/mod_iterations")
 
-richness_data_europe <-
-  richness_data_europe %>% 
-  mutate(region = "europe")
+#----------------------------------------------------------#
+# 4. Standardize richness  ----
+#----------------------------------------------------------#
 
-richness_data_namerica <-
-  richness_data_namerica %>%  
-  mutate(region = "namerica") 
+standardize_richness <- purrr::map(
+           .x = paths,
+           .f = ~ {
+             readr::read_rds(.x) %>%
+               dplyr::group_by(region) %>%
+               dplyr::mutate(st_richness = scale(richness)[, 1]) %>%
+               dplyr::ungroup() %>%
+               dplyr::mutate(
+                 region = as.factor(region),
+                 dataset_id = as.factor(dataset_id))
+           }
+         )
 
-#bind all dataframes with richness estimate, convert dataset_id as random factor and add regions
-
-study3_richness <- 
-  bind_rows(richness_data_asia,richness_data_europe, richness_data_namerica) %>%
-  mutate(dataset_id = as_factor(dataset_id))
-
-# standardize diversity use scale():
-
-study3_richness_z_scores <- 
-  study3_richness %>% 
-  group_by(region) %>% 
-  mutate(st_richness = scale(richness)) %>%
-  ungroup() %>% 
-  mutate(st_richness = st_richness[,1]) %>% 
-  mutate(region = as_factor(region))
 
 # Get mean and sd to back-transform 
 
-study3_richness_standard <-
-  study3_richness %>% 
-  dplyr::group_by(region) %>% 
-  dplyr::summarise(mean_richness = mean(richness, na.rm = TRUE), 
-                   sd_richness = sd(richness, na.rm = TRUE)) %>% 
-  dplyr::ungroup()
+study3_richness_sd <- purrr::map(
+            .x = standardize_richness,
+            .f = ~{
+              .x %>% 
+              dplyr::group_by(region) %>% 
+              dplyr::summarise(mean_richness = mean(richness, na.rm = TRUE), 
+              sd_richness = sd(richness, na.rm = TRUE)) %>% 
+              dplyr::ungroup()
+            }
+          )
 
 #----------------------------------------------------------#
-# 3. Model fitting -----
+# 5. Model fitting -----
 #----------------------------------------------------------#
 
-## 3.1. Parallel processing -----
+## 5.1. Parallel processing -----
 
 n_available_cores <-
   parallelly::availableCores() - 1
 
 # number of cores to use cannot be more than number of random effect levels
 n_cores_to_use <-
-  study3_richness_z_scores %>%
+  standardize_richness[[1]] %>%
   dplyr::distinct(dataset_id) %>%
   nrow() %>%
   {
@@ -104,253 +115,117 @@ n_cores_to_use <-
   } %>%
   min(., n_available_cores)
 
-## 3.2. Fit model -----
+## 5.2. Fit model -----
 
 set.seed(19900723)
 
-gam_s3 <-
-  fit_regression_model(
-    data = study3_richness_z_scores,
-    y_var = "st_richness",
-    time_var = "age",
-    group_var = "region",
-    random = "study3",
-    sel_k = 12, 
-    error_family = scat(),
-    nthreads = n_cores_to_use,
-    discrete = TRUE,
-    control = mgcv::gam.control(
-      trace = TRUE,
-      maxit = 500
-    )
-  )
-
-## 3.3. Save model as RDS files --
-
-write_rds(gam_s3,here("Data/Paper_1/data_model/gam_3.rds"))
-
-#----------------------------------------------------------#
-# 4. Model prediction -----
-#----------------------------------------------------------#
-
-data_dummy_full <-
-  tidyr::expand_grid(
-    study3_richness_z_scores %>% 
-      distinct(region),
-    age = seq(
-      min(study3_richness_z_scores$age),
-      max(study3_richness_z_scores$age),
-      length.out = 100
-    )
-  )
-
-data_pred_full <-
-  predict_model(
-    model = gam_s3,
-    newdata = data_dummy_full,
-    type = "response",
-    exclude_terms = "dataset_id"
-  ) %>%
-  as.data.frame() %>%
-  tibble::as_tibble() %>%
-  dplyr::relocate(
-    estimate, region, age,
-    .before = dplyr::everything()
-  )
-
-data_richness_btr <- 
-  data_pred_full %>% 
-  left_join(study3_richness_standard, by = "region") %>% 
-  mutate(
-    richness = estimate*sd_richness + mean_richness,
-    rich_low = conf_low*sd_richness + mean_richness,
-    rich_high = conf_high*sd_richness + mean_richness) 
-  
-
-#----------------------------------------------------------#
-# 4. Visualization -----
-#----------------------------------------------------------#
-
-##general plot
-
-p <-
-  ggplot2::ggplot(
-  ) +
-  ggplot2::labs(
-    y = "Pollen Richness", x = "cal yr BP") +
-  ggplot2::theme_classic(
+purrr::walk2(
+  .progress = TRUE,
+  .x = standardize_richness,
+  .y = seq_along(standardize_richness),
+  .f = ~ {
+    out_file <- file.path(out_dir_mod, paste0("model_", .y, ".rds"))
     
-  )+
-  ggplot2::theme(legend.position = "none",
-                 plot.title = element_text(color = "#2a707f"),
-                 axis.title = element_text(color = "#2a707f", size = 18),
-                 axis.text  = element_text(color = "#2a707f", size = 24),
-                 axis.ticks = element_line(color = "#2a707f"),
-                 axis.line  = element_line(color = "#2a707f", linewidth = 1)
-  )
+    # skip if iteration already ran
+    if (file.exists(out_file)) {
+      message("Skipping: ", .y)
+      return(NULL)
+    }
+    
+    message("Running: ", .y)
+    
+    model <- fit_regression_model(
+      data_source = .x,
+      y_var = "st_richness",
+      time_var = "age",
+      group_var = "region",
+      random = "intercept_reg",
+      sel_k = 12,
+      error_family = scat(),
+      nthreads = n_cores_to_use,
+      discrete = TRUE,
+      control = mgcv::gam.control(
+        trace = FALSE,
+        maxit = 500
+      )
+    )
+    
+    readr::write_rds(model, out_file)
+  }
+)
 
-#  4.1. Plot predictions for each region -----
+# View a single iteration
 
-#show continental trend in one figure
-
-p1 <- p +
-  ggplot2::facet_wrap(~ region, dir = 'rt', ncol = 1, strip.position = 'right') +
-  ggplot2::geom_ribbon(
-    data = data_richness_btr,
-    ggplot2::aes(
-      x = age,
-      y = richness,
-      ymin = rich_low,
-      ymax = rich_high,
-      fill = as.factor(region)
-    ),
-    alpha = 0.3
-  ) +
-  ggplot2::geom_line(
-    data = data_richness_btr,
-    ggplot2::aes(x = age, y = richness,color = region),
-    linewidth = 1
-  ) +
-  ggplot2::theme(legend.position = "none",
-                 axis.text  = element_text(size = 5),
-                 strip.text = element_text(
-                   size = 10,
-                   color = "#2a707f"
-                 ),
-                 strip.background = element_rect(
-                   color = "#2a707f",
-                   fill = NA,
-                   linewidth = 0.3
-                 )
-  ) +
-  ggplot2::coord_cartesian(
-    ylim = c(14, 24) 
-  ) 
-
-p1 +
-  ggplot2::scale_x_reverse()
+one <- 
+  readr::read_rds(here::here("Data/Paper_1/data_model/mod_iterations/model_1.rds"))
 
 
-## show individual trend for each continent
+#----------------------------------------------------------#
+# 5. Save  files for prediction -----
+#----------------------------------------------------------#
 
-asia <- data_richness_btr %>% filter(region == 'asia')
-europe <- data_richness_btr %>% filter(region == 'europe')
-namerica <- data_richness_btr %>% filter(region == 'namerica')
+readr::write_rds(standardize_richness,here("Data/Paper_1/data_estimate_richness/standardized_richness.rds"))
 
-##Asia
+readr::write_rds(study3_richness_sd,here("Data/Paper_1/data_estimate_richness/study3_richness_sd.rds"))
 
-A <- p +
-  ggplot2::geom_ribbon(
-    data = asia,
-    ggplot2::aes(
-      x = age,
-      y = richness,
-      ymin = rich_low,
-      ymax = rich_high,
-      fill = region
-    ),
-    alpha = 0.1
-  ) +
-  ggplot2::geom_line(
-    data = asia,
-    ggplot2::aes(x = age, y = richness),
-    linewidth = 4, color = 'red'
-  ) +
-  ggplot2::theme(legend.position = "none",
-                 axis.text  = element_text(size = 22),
-                 strip.text = element_text(
-                   size = 16,
-                   color = "#2a707f"
-                 ),
-                 strip.background = element_rect(
-                   color = "#2a707f",
-                   fill = NA,
-                   linewidth = 0.3
-                 )
-  ) +
-  ggplot2::coord_cartesian(ylim = c(14, 19) 
-  ) + 
-  ggplot2::scale_x_reverse()
 
-A
 
-#Europe
 
-E <- p +
-  ggplot2::geom_ribbon(
-    data = europe,
-    ggplot2::aes(
-      x = age,
-      y = richness,
-      ymin = rich_low,
-      ymax = rich_high,
-      fill = region
-    ),
-    alpha = 0.1
-  ) +
-  ggplot2::geom_line(
-    data = europe,
-    ggplot2::aes(x = age, y = richness),
-    linewidth = 4, color = 'purple'
-  ) +
-  ggplot2::theme(legend.position = "none",
-                 axis.text  = element_text(size = 22),
-                 strip.text = element_text(
-                   size = 10,
-                   color = "#2a707f"
-                 ),
-                 strip.background = element_rect(
-                   color = "#2a707f",
-                   fill = NA,
-                   linewidth = 0.3
-                 )
-  ) +
-  ggplot2::coord_cartesian(ylim = c(14, 22) 
-  ) + 
-  ggplot2::scale_x_reverse()
+###ALTERNATIVE (without iteration) --
 
-E
 
-##NAmerica
+#Load richness
 
-N <- p +
-  ggplot2::geom_ribbon(
-    data = namerica,
-    ggplot2::aes(
-      x = age,
-      y = richness,
-      ymin = rich_low,
-      ymax = rich_high,
-      fill = region
-    ),
-    alpha = 0.1
-  ) +
-  ggplot2::geom_line(
-    data = namerica,
-    ggplot2::aes(x = age, y = richness),
-    linewidth = 4, color = 'orange'
-  ) +
-  ggplot2::theme(legend.position = "none",
-                 axis.text  = element_text(size = 22),
-                 strip.text = element_text(
-                   size = 10,
-                   color = "#2a707f"
-                 ),
-                 strip.background = element_rect(
-                   color = "#2a707f",
-                   fill = NA,
-                   linewidth = 0.3
-                 )
-  ) +
-  ggplot2::coord_cartesian(ylim = c(16, 18) 
-  ) + 
-  ggplot2::scale_x_reverse()
+richness_data <- purrr::map(
+  .x = paths,
+  .f = ~{
+    data <- .x %>% 
+      readr::read_rds()
+    }
+) %>% 
+  purrr::list_rbind()
 
-N
+#standardize richness
 
-##combine continental trends into single plot
+richness_std <- 
+  richness_data%>%
+  dplyr::group_by(region) %>%
+  dplyr::mutate(st_richness = scale(richness)[, 1]) %>%
+  dplyr::ungroup() %>%
+  dplyr::mutate(
+    region = as.factor(region),
+    dataset_id = as.factor(dataset_id))
 
-library(patchwork)
+# get sd
 
-A + E + N
+richness_sd <- 
+  richness_std %>% 
+  dplyr::group_by(region) %>% 
+  dplyr::summarise(mean_richness = mean(richness, na.rm = TRUE), 
+    sd_richness = sd(richness, na.rm = TRUE)) %>% 
+  dplyr::ungroup()
 
+
+##fit model (k = 50)
+
+model_0 <- fit_regression_model(  #ran 5pm 4/6/2026 done 5:30 pm
+  progress = "tk",
+  data_source = richness_std,
+  y_var = "st_richness",
+  time_var = "age",
+  group_var = "region",
+  random = "intercept_reg",
+  sel_k = 50,
+  error_family = scat(),
+  nthreads = n_cores_to_use,
+  discrete = TRUE,
+  control = mgcv::gam.control(
+    trace = FALSE,
+    maxit = 500
+  ))
+
+##save richness_std, richness_sd, model
+
+readr::write_rds(model_0, here::here("Data/Paper_1/data_model/model_0.rds"))
+readr::write_rds(richness_std, here::here("Data/Paper_1/data_estimate_richness/s3_richness_std_no_iter.rds"))
+readr::write_rds(richness_sd, here::here("Data/Paper_1/data_estimate_richness/s3_richness_sd_no_iter.rds"))
