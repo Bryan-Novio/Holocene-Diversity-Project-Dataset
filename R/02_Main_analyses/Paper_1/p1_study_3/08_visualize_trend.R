e9ed1ee95f2674782b04f24fb02ffa5ec72a7561
Bryan Novio <noviobryan@gmail.com>
2026-05-14 22:25:17 +0200

    revised visualisation of summarised trends for study 3

--- R/02_Main_analyses/Paper_1/p1_study_3/08_visualize_trend.R
+++ R/02_Main_analyses/Paper_1/p1_study_3/08_visualize_trend.R
@@ -15,35 +15,11 @@
 library(tidyverse)
 library(here)
 
+
 #----------------------------------------------------------#
 # 1. Set -up -----------------------------------------
 #----------------------------------------------------------# 
-
-## 1.1. Load data set
-
-purrr::map(1:10, 
-           .progress = TRUE,
-           .f = load_pred_richness_and_select) %>% 
-  bind_rows() %>% 
-  group_by(region, age) %>% 
-  summarise(
-    conitental_median_richness = median(richness),
-    conitental_richness_upp = quantile(richness, 0.975),
-    conitental_richness_dwn = quantile(richness, 0.025)
-  )
-
-data_rich <- list.files(
-  "Data/Paper_1/data_estimate_richness/s3_richness",
-  pattern = "[.]rds$",
-  full.names = TRUE
-)
-
-data_back <- list.files(
-  "Data/Paper_1/data_model/data_back",
-   pattern = "[.]rds$",
-   full.names = TRUE)
-
-## 1.2. Load functions
+## 1.1. Load functions
 
 # Load the function into the global environment
 
@@ -60,13 +36,93 @@ source_files <-
     source
   )
 
+## 1.2. Load data set
+
+median_richness_back <- 
+  purrr::map(1:1000, 
+           .progress = TRUE,
+           .f = load_pred_richness_and_select) %>% 
+  bind_rows() %>% 
+  group_by(region, age) %>% 
+  summarise(
+    continental_median_richness = median(richness),
+    continental_richness_upp = quantile(richness, 0.975),
+    continental_richness_dwn = quantile(richness, 0.025)
+  )
+
+data_rich <- list.files(
+  "Data/Paper_1/data_estimate_richness/s3_richness",
+  pattern = "[.]rds$",
+  full.names = TRUE
+)
+
+data_back <- list.files(
+  "Data/Paper_1/data_model/data_back",
+  pattern = "[.]rds$",
+  full.names = TRUE)
+
+
 #----------------------------------------------------------#
-# 2. Visualize trends --
+# 2. Visualise continental trends -------------------------
+#----------------------------------------------------------# 
+
+# All continents in one plot
+
+median_richness_back %>% 
+  ggplot(aes(x = age, y = continental_median_richness)) +
+  geom_line(linewidth = 1, color = "red") + 
+  geom_ribbon(aes(ymin = continental_richness_dwn, 
+              ymax = continental_richness_upp),  fill = "blue", alpha = 0.4) +
+  facet_wrap(~region) +
+  theme_classic()+
+  scale_x_reverse()
+
+
+#Europe
+
+median_richness_back %>% 
+  filter(region == "Europe") %>% 
+  ggplot(aes(x = age, y = continental_median_richness)) +
+  geom_line(linewidth = 1, color = "red") + 
+  geom_ribbon(aes(ymin = continental_richness_dwn, 
+                  ymax = continental_richness_upp),  fill = "blue", alpha = 0.4) +
+  labs(x = "Age(cal yr BP)" , y = "Richness") +
+  theme_classic()+
+  scale_x_reverse()
+
+# Asia
+
+median_richness_back %>% 
+  filter(region == "Asia") %>% 
+  ggplot(aes(x = age, y = continental_median_richness)) +
+  geom_line(linewidth = 1, color = "red") + 
+  geom_ribbon(aes(ymin = continental_richness_dwn, 
+                  ymax = continental_richness_upp),  fill = "blue", alpha = 0.4) +
+  labs(x = "Age(cal yr BP)" , y = "Richness") +
+  theme_classic()+
+  scale_x_reverse()
+
+# N. America
+
+
+median_richness_back %>% 
+  filter(region == "North America") %>% 
+  ggplot(aes(x = age, y = continental_median_richness)) +
+  geom_line(linewidth = 1, color = "red") + 
+  geom_ribbon(aes(ymin = continental_richness_dwn, 
+                  ymax = continental_richness_upp),  fill = "blue", alpha = 0.4) +
+  labs(x = "Age(cal yr BP)" , y = "Richness") +
+  theme_classic()+
+  scale_x_reverse()
+
+
+#----------------------------------------------------------#
+# 3. Visualize trends per iteration with site-level trends --
 #----------------------------------------------------------# 
 
 ##2.1. for single iteration
 
-readr::read_rds(rich_data[[1]]) %>% summary()
+readr::read_rds(data_rich[[1]]) %>% summary()
 
 ### e.g. for iteration 1
 
@@ -94,5 +150,4 @@ all_data_back_iters <- purrr::map(
 )
 
 plot_richness_iter(all_data_back_iters,data_rich,1)
-
 
